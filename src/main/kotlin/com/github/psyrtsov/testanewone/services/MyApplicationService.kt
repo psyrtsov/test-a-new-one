3dd4c9739cfe4954e22ef4549487dd7af0aab320
GitHub Action <action@github.com>
2022-08-20 01:44:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/psyrtsov/testanewone/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/psyrtsov/testanewone/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.psyrtsov.testanewone.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.psyrtsov.testanewone.MyBundle
 
 class MyApplicationService {
 
